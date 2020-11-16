72ed71d0828fda0096047aba09f2969a850172f0
Cherqaoui mehdi <cherqaoui.elmehdi@gmail.com>
2020-11-16 02:12:00 +0100

    add some colors

--- src/styles/colors.js
+++ src/styles/colors.js
@@ -1,11 +1,13 @@
 const colors = {
   navy: '#0a192f',
   lightNavy: '#172a45',
+  lightestNavy: '#303C55',
   transparentNavy: '#0a192fd9',
   navyShadow: '#020c1b',
   grey: '#4c5772',
   slate: '#8892b0',
   lightSlate: '#a8b2d1',
+  lightestSlate: '#ccd6f6',
   green: '#64ffda',
   lightGreen: '#143041'
 }
