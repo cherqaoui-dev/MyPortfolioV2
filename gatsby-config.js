870660a00f2a800198edd43e68f10eee04dfbbd7
Cherqaoui mehdi <cherqaoui.elmehdi@gmail.com>
2020-11-24 04:41:45 +0100

    Added Plugins

--- gatsby-config.js
+++ gatsby-config.js
@@ -17,6 +17,7 @@ module.exports = {
     twitterUsername: site.twitterUsername,
   },
   plugins: [
+    'gatsby-plugin-styled-components',
     'gatsby-plugin-react-helmet',
     'gatsby-transformer-sharp',
     'gatsby-plugin-sharp',
